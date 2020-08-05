a4da9b16181d88de4e9be480a803e4c8178733f3
Sunny <thstkd92@gmail.com>
2020-08-05 11:13:05 +0900

    스프링 시큐리티: 폼 인증 7. 시큐리티 테스트

--- src/test/java/com/practice/springsecurity/account/AccountControllerTest.java
+++ src/test/java/com/practice/springsecurity/account/AccountControllerTest.java
@@ -2,15 +2,18 @@ package com.practice.springsecurity.account;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithAnonymousUser;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -22,6 +25,9 @@ class AccountControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    private AccountService accountService;
 
     @Test
     @WithAnonymousUser
@@ -51,5 +57,24 @@ class AccountControllerTest {
         mockMvc.perform(get("/").with(user("sunny").roles("admin")))
                 .andDo(print())
                 .andExpect(status().isOk());
+    }
+
+    @CsvSource({"sunny, abc, USER"})
+    @ParameterizedTest
+    public void login(String username, String password, String role) throws Exception {
+        Account user = createUser(username, password, role);
+
+        mockMvc.perform(formLogin().user(user.getUsername()).password(password))
+                .andExpect(authenticated());
+    }
+
+    private Account createUser(String username, String password, String role) {
+        Account account = new Account();
+        account.setUsername(username);
+        account.setPassword(password);
+        account.setRole(role);
+        accountService.createNew(account);
+
+        return account;
     }
 }
