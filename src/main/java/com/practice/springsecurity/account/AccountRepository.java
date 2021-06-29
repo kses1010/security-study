da1bc9bbecfdfbc031adc4303a5caff4440aebe9
Sunny <thstkd92@gmail.com>
2021-06-29 23:00:22 +0900

    스프링 시큐리티 스터디 0. 시큐리티 인강을 위한 초기화

--- src/main/java/com/practice/springsecurity/account/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.practice.springsecurity.account;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Integer> {
-    Account findByUsername(String username);
-}
