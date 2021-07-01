94f0dabf3f4fbed7e7a4a25a235965a50f8c9629
Sunny <thstkd92@gmail.com>
2021-07-01 22:18:37 +0900

    스프링 시큐리티 스터디 4. Logout 필터

--- src/main/java/com/practice/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/practice/springsecurity/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.servlet.http.HttpSession;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -13,21 +15,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(requests -> requests.anyRequest().authenticated());
 
-        http.formLogin()
-                .loginPage("/loginPage")
-                .defaultSuccessUrl("/")
-                .failureUrl("/login")
-                .usernameParameter("userId")
-                .passwordParameter("secret")
-                .loginProcessingUrl("/login-proc")
-                .successHandler((request, response, authentication) -> {
-                    System.out.println("authentication " + authentication.getName());
-                    response.sendRedirect("/");
+        http.formLogin();
+
+        http.logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .addLogoutHandler((request, response, authentication) -> {
+                    HttpSession session = request.getSession();
+                    session.invalidate();
                 })
-                .failureHandler(((request, response, exception) -> {
-                    System.out.println("exception " + exception.getMessage());
+                .logoutSuccessHandler((request, response, authentication) -> {
                     response.sendRedirect("/login");
-                }))
-                .permitAll();
+                })
+                .deleteCookies("remember-me");
     }
 }
